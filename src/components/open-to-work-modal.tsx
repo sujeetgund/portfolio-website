20ff21061ab699612c6826254ad47321de2e0e29
Sujeet Gund <sujeetgund@gmail.com>
2025-09-11 05:12:49 +0000

    I made own changes to make modal content background white in color.  But

--- src/components/open-to-work-modal.tsx
+++ src/components/open-to-work-modal.tsx
@@ -43,7 +43,7 @@ export function OpenToWorkModal() {
   
   const handleOpenToWorkClick = () => {
     handleDismiss();
-    window.location.href = 'mailto:sujeetgund@email.com';
+    window.location.href = 'mailto:sujeetgund@gmail.com';
   }
 
   return (
